2158877b5f146735d04e1cac4bab1a184f4e4206
ehsan1381 <ehsan.tc.evol@gmail.com>
2026-08-08 11:45:14 -0700

    enabling all of the 'tests'

--- integration/src/test.m
+++ integration/src/test.m
@@ -1,23 +1,23 @@
 tolerance = 1e-12;
 nParts = 100;
 
-%f = @(x)(abs(x));
-%interval = [-1, 1];
-%[integralApproximation, nPartitions ]  = integration(f, interval, tolerance, nParts)
+f = @(x)(abs(x));
+interval = [-1, 1];
+[integralApproximation, nPartitions ]  = integration(f, interval, tolerance, nParts)
 %
-%f = @(x)(log(x));
-%interval = [1, exp(1)];
-%[integralApproximation, nPartitions ]  = integration(f, interval, tolerance, nParts)
-%
-%
-%f = @(x)(cos(x));
-%interval = [0, pi/2];
-%[integralApproximation, nPartitions ]  = integration(f, interval, tolerance, nParts)
+f = @(x)(log(x));
+interval = [1, exp(1)];
+[integralApproximation, nPartitions ]  = integration(f, interval, tolerance, nParts)
 %
 %
-%f = @(x)(sin(x));
-%interval = [ -pi/2, pi/2];
-%[integralApproximation, nPartitions ]  = integration(f, interval, tolerance, nParts)
+f = @(x)(cos(x));
+interval = [0, pi/2];
+[integralApproximation, nPartitions ]  = integration(f, interval, tolerance, nParts)
+%
+%
+f = @(x)(sin(x));
+interval = [ -pi/2, pi/2];
+[integralApproximation, nPartitions ]  = integration(f, interval, tolerance, nParts)
 %
 
 f = @(x)(1./(sqrt(1-log(x))));
